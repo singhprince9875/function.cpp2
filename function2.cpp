3eae379399207ca137d40d1c53e01ffaffba59be
Prince kumar Singh <122429010+singhprince9875@users.noreply.github.com>
2023-03-27 10:59:55 +0530

    Add files via upload

---
+++ function2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int oddnumber( ){
+  if(p % 2=0 ){
+    return = "even number";
+    cout<<"even number"<<endl;
+  }
+}
+
+int main(){
+    oddnumber(9);
+}
